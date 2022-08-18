baa5a6e852b085a9eb093b3634ef78dfc4f7cada
Mudasir-Majeed <111265994+Mudasir-Majeed@users.noreply.github.com>
2022-08-18 03:57:51 -0400

    Add via upload

---
+++ test.py
@@ -0,0 +1,24 @@
+# Basics of Python
+print("Hello World")
+a=20;
+b=30;
+sum=a+b;
+print("Sum of two numbers is", sum);
+print("*******************")
+""" it is a multiline comment
+print("i am a dc")
+2+3
+print("Hi i am a programmer")
+print("I am a learner")
+"""
+# variables in python
+a=30;
+b=90;
+print(a);
+print(b);
+x=str(3);
+y=int(3);
+z=float(3);
+print(x)
+print(y)
+print(z)
